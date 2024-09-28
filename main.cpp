29b9fd3e26320986cb7727ef223c48cf191cbd7e
Samuele-Scandellari <samuscan01@gmail.com>
2024-09-28 15:47:17 +0200

    Esercizio 3.2

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int numero;
+    printf("Determina se il numero è postivo o negativo\nnumero: ");
+    scanf("%d", &numero);
+    if (numero >=0);
+    printf("Il numero è positivo\n");
+    else
+    printf("Il numero è negativo\n");
+}
